d9cf63ff861867134c90679499005b4755525e8b
Winnie_Onyancha <wonyancha22@gmail.com>
2023-05-04 05:07:48 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,23 @@
-char* _strcat(char* dest, char* src) {
-    char* ptr = dest;
-    // Find the end of dest
-    while (*ptr != '\0') {
-        ptr++;
-    }
-    // Append src to dest
-    while (*src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-    // Add a terminating null byte
-    *ptr = '\0';
-    return dest;
+
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
 }
