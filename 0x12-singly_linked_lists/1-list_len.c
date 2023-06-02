ba1951b2420963cceb8c4fee63368f08d4faacbb
Winnie_Onyancha <wonyancha22@gmail.com>
2023-06-02 11:32:20 -0700

    Gpt

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,12 @@
-#include <stddef.h>
 #include "lists.h"
+/**
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
 
 size_t list_len(const list_t *h) {
-    size_t count = 0;
+size_t count = 0;
     const list_t *current = h;
 
     while (current != NULL) {
