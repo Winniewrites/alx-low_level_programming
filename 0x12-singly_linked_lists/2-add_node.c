cd7842944a1f49cd2dab5f136d923fc3e4b9d83a
Winnie_Onyancha <wonyancha22@gmail.com>
2023-06-02 11:46:24 -0700

    gpt2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include "lists.h"
 
-
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
@@ -27,7 +26,6 @@ list_t *add_node(list_t **head, const char *str)
 	new->next = (*head);
 	(*head) = new;
 
-	
 	return (*head);
 }
 
